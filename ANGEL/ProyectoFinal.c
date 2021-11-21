4164d31487619ac379b807ac3d03aef24fb3249e
Angel Pineda <lamostace@outlook.com>
2021-11-21 17:05:50 -0600

    UPDATE

--- ANGEL/ProyectoFinal.c
+++ ANGEL/ProyectoFinal.c
@@ -6,21 +6,6 @@
 #include <windows.h>
 #include <stdbool.h>
 
-/*
-El programa deberá pedir los datos del cliente, su dirección y los datos de su auto, 
-una vez registrados los datos anteriores, el programa deberá de mostrar una pantalla con los
-distintos mantenimientos y reparaciones que el taller puede realizar.
-
-Al seleccionar uno o más de ellos el sistema deberá mostrar los precios unitarios y
-el total de la reparación.
-
-Una vez que se termine con la selección de los mantenimientos y/o
-repataciones, el programa deberá preguntar si se procede con el trabajo o no.
-En caso de "NO", solo mostrar en pantalla e resumen y salir. En caso de que "SI", el 
-programa deberá de mostrar una fecha de entreha, así como el resumen del trabajo con sus costos,
-esto último será guardado en un archivo de texto.
-*/
-
 typedef struct data
 {
     char nombre[80];
@@ -46,11 +31,12 @@ typedef struct servicios
     bool engrasado;
     bool rotacion;
     bool frenos;
+    float subtotal;
 }SERVICIOS;
 
 
 
-DATOS getInfo(DATOS *cliente) {
+DATOS getInfo(DATOS *cliente ) {
     system("cls");
     printf("Ingrese su nombre completo en mayusculas: ");
     fflush(stdin);
@@ -73,18 +59,18 @@ DATOS getInfo(DATOS *cliente) {
     return *cliente;
 }
 
-int listado(SERVICIOS *resumen) {
+int listado(SERVICIOS *resumen ) {
     int OPCION;
     bool flag = true;
-    float subtotal = 0.0;
+    resumen->subtotal = 0.0;
     while (flag)
     {
         system("cls");
         printf("	        Seleccione un servicio a realizar.:\n");
         printf("--------------------------------------------------------------------\n");
-        printf("        SERVICIO                                        IMPORTE\n");
+        printf("        SERVICIO                                          IMPORTE\n");
         printf("--------------------------------------------------------------------\n");
-		printf("1.-Lubricació y cambio de aceite                        $3,000.0 MXN\n");
+		printf("1.-Lubricación y cambio de aceite                       $3,000.0 MXN\n");
 		printf("2.-Reemplazo de acumulador                              $2,000.0 MXN\n");
 		printf("3.-Suspension                                           $4,000.0 MXN\n");
 		printf("4.-Alineacion y Balanceo                                $700.0   MXN\n");
@@ -96,44 +82,44 @@ int listado(SERVICIOS *resumen) {
 		printf("10.-Rotacion                                            $1,000.0 MXN\n");
 		printf("11.-Revision,limpieza y ajuste de frenos                $1,000.0 MXN\n");
         printf("--------------------------------------------------------------------\n");
-        printf("                                              Subtotal: $%.1f\n",subtotal);
+        printf("                                              Subtotal: $%.1f\n",resumen->subtotal);
 		printf("12 para finalizar.\n");
         printf("Seleccione: "); scanf("%d",&OPCION);
         fflush(stdin);
         switch (OPCION)
         {
         case 1:
-            resumen->aceite = true; subtotal+= 3000.0;
+            resumen->aceite = true; resumen->subtotal+= 3000.0;
             break;
         case 2:
-            resumen->acumulador = true; subtotal+= 2000.0;
+            resumen->acumulador = true; resumen->subtotal+= 2000.0;
             break;
         case 3:
-            resumen->suspension = true;subtotal+= 4000.0;
+            resumen->suspension = true;resumen->subtotal+= 4000.0;
             break;
         case 4:
-            resumen->alineacion = true;subtotal+= 700.0;
+            resumen->alineacion = true;resumen->subtotal+= 700.0;
             break;
         case 5:
-            resumen->mofle = true;subtotal+= 5000.0;
+            resumen->mofle = true;resumen->subtotal+= 5000.0;
             break;
         case 6:
-            resumen->torneado = true;subtotal+= 1000.0;
+            resumen->torneado = true;resumen->subtotal+= 1000.0;
             break;
         case 7:
-            resumen->clutch = true;subtotal+= 2000.0;
+            resumen->clutch = true;resumen->subtotal+= 2000.0;
             break;
         case 8:
-            resumen->diferenciales = true;subtotal+= 2000.0;
+            resumen->diferenciales = true;resumen->subtotal+= 2000.0;
             break;
         case 9:
-            resumen->engrasado = true;subtotal+= 500.0;
+            resumen->engrasado = true;resumen->subtotal+= 500.0;
             break;
         case 10:
-            resumen->rotacion = true; subtotal+= 1000.0;
+            resumen->rotacion = true; resumen->subtotal+= 1000.0;
             break;
         case 11:
-            resumen->frenos = true;subtotal+= 1000.0;
+            resumen->frenos = true;resumen->subtotal+= 1000.0;
             break;
         case 12:
             flag = false;
@@ -146,10 +132,101 @@ int listado(SERVICIOS *resumen) {
     
 }
 
+int resumenfinal(SERVICIOS *resumen, FILE *fp, DATOS *cliente) {
+    
+    system("cls");
+    fprintf(fp,"Mexicanicos S.A de C.V\n");
+    fprintf(fp,"*********************************************************************\n");
+    fprintf(fp,"Cliente: %s\n",cliente->nombre);
+    fprintf(fp,"Teléfono: %s\n",cliente->tel);
+    fprintf(fp,"Dirección: %s\n",cliente->Direccion);
+    fprintf(fp,"\nAutomóvil\n");
+    fprintf(fp,"Marca: %s\n",cliente->Marca);
+    fprintf(fp,"Modelo: %s\n",cliente->Modelo);
+    fprintf(fp,"Año: %d\n",cliente->year);
+    fprintf(fp,"*********************************************************************\n");
+    fprintf(fp,"Servicios a realizar: \n");
+
+
+    printf("*********************************************************************\n");
+    if (resumen->aceite){
+       printf("-Lubricación y cambio de aceite                        $3,000.0 MXN\n");
+       fprintf(fp,"-Lubricación y cambio de aceite                        $3,000.0 MXN\n");
+    }
+    if (resumen->acumulador)
+    {
+       printf("-Reemplazo de acumulador                              $2,000.0 MXN\n");
+       fprintf(fp,"-Reemplazo de acumulador                               $2,000.0 MXN\n");
+    }
+    if (resumen->suspension)
+    {
+       printf("-Alineacion y Balanceo                                $700.0   MXN\n");
+       fprintf(fp,"-Alineacion y Balanceo                                 $700.0   MXN\n");
+    }
+    if (resumen->clutch)
+    {
+       printf("-Servicio de clutch                                   $2,000.0 MXN\n");
+       fprintf(fp,"-Servicio de clutch                                   $2,000.0 MXN\n");
+    }
+    if (resumen->mofle)
+    {
+       printf("-Mofles                                               $5,000.0 MXN\n");
+       fprintf(fp,"-Mofles                                                $5,000.0 MXN\n");
+    }
+    if (resumen->torneado)
+    {
+       printf("-Rectificacion y Torneado                             $1,000.0 MXN\n");
+       fprintf(fp,"-Rectificacion y Torneado                              $1,000.0 MXN\n");
+    }
+    if (resumen->alineacion)
+    {
+       printf("-Alineacion y Balanceo                                $700.0   MXN\n");
+       fprintf(fp,"-Alineacion y Balanceo                                 $700.0   MXN\n");
+    }
+    if (resumen->diferenciales)
+    {
+       printf("-Sevicio de diferenciales                             $2,000.0 MXN\n");
+       fprintf(fp,"-Sevicio de diferenciales                              $2,000.0 MXN\n");
+    }
+    if (resumen->engrasado)
+    {
+       printf("-Engrasado                                            $500.0   MXN\n");
+       fprintf(fp,"-Engrasado                                             $500.0   MXN\n");
+    }
+    if (resumen->rotacion)
+    {
+       printf("-Rotacion                                            $1,000.0 MXN\n");
+       fprintf(fp,"-Rotacion                                              $1,000.0 MXN\n");
+    }
+    if (resumen->frenos)
+    {
+       printf("-Revision,limpieza y ajuste de frenos                $1,000.0 MXN\n");
+       fprintf(fp,"-Revision,limpieza y ajuste de frenos                  $1,000.0 MXN\n");
+    }
+    printf("*********************************************************************\n");
+    printf("SUBTOTAL: $%.1f\n",resumen->subtotal);
+    printf("\nSu recibo ha sido impreso en un archivo.\n");
+    printf("*********************************************************************\n");
+    printf("\n¡Gracias por su preferencia!");
+
+
+
+    fprintf(fp,"*********************************************************************\n");
+    fprintf(fp,"SUBTOTAL: $%.1f MXN\n",resumen->subtotal);
+    fprintf(fp,"\n¡Gracias por su preferencia!");
+
+
+
+    
+}
+
 int main(){
     SetConsoleOutputCP(CP_UTF8);
     setlocale(LC_ALL, "es_ES");
     
+    FILE *fp;
+    fp = fopen("Resumen.txt","w");
+    int OPC;
     int OPC2;
     DATOS cliente;
     DATOS *puntero = &cliente;
@@ -169,7 +246,6 @@ int main(){
     printf("Bienvenido al Mexicanico\n");
     printf("************************\n");
     printf("¿Desea realizar alguna cita o consulta?\n  1.Si\n  2.No\n");
-    int OPC;
     scanf("%d",&OPC);
     switch (OPC)
     {
@@ -184,12 +260,12 @@ int main(){
         switch (OPC2)
         {
         case 1:
-            /* code */
+            resumenfinal(respuntero,fp,puntero);
             break;
         
         default:
             system("cls");
-            printf("¡Vuelva pronto!\nMexicanicos te espera.")
+            printf("¡Vuelva pronto!\nMexicanicos te espera.");
             break;
         }
         break;
